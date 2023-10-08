cf7b3f2155304762991d41f63d4d9c9447f886dc
ƛɗɩtyɑ Ɦɑɭɗɘr <144584943+AdityaHalder@users.noreply.github.com>
2023-10-09 00:51:03 +0530

    Create gdel.py

---
+++ AdityaHalder/plugins/gdel.py
@@ -0,0 +1,111 @@
+# Powered By @BadBoyKaaL // @AdityaHalder
+
+import asyncio
+
+from pyrogram import *
+from pyrogram.types import Message
+
+from .. import *
+from ..vars import GDEL_USERS
+from ..modules.dbase import (
+    is_gdel_user,
+    get_gdel_user,
+    get_gdel_count,
+    add_gdel_user,
+    del_gdel_user
+)
+
+
+@app.on_message(command(["gdl", "gdel", "gdelete"]) & filters.user(SUPER_USERS))
+async def add_gdelete_user(client, message: Message):
+    if not message.reply_to_message:
+        if len(message.command) != 2:
+            return await message.reply_text("Reply to a user's message or give username/user_id.")
+        user = message.text.split(None, 1)[1]
+        user = await app.get_users(user)
+        user_id = user.id
+        mention = user.mention
+    else:
+        user_id = message.reply_to_message.from_user.id
+        mention = message.reply_to_message.from_user.mention
+    if user_id == message.from_user.id:
+        return await message.reply_text("You want to add Global Delete yourself? How Fool!")
+    elif user_id == SUPUSER:
+        return await message.reply_text("Should i activate Global Delete on myself? Lol")
+    elif user_id in SUDOERS:
+        return await message.reply_text("You want add Global Delete on sudo user?")
+    is_gdel = await is_gdel_user(user_id)
+    if is_gdel:
+        return await message.reply_text("{0} is already affected by **Global Delete**".format(mention))
+    if user_id not in GDEL_USERS:
+        GDEL_USERS.add(user_id)
+    await add_gdel_user(user_id)
+    await message.reply_text("**Global Delete Activated.")
+
+
+
+@app.on_message(command(["ungdl", "ungdel", "ungdelete"]) & filters.user(SUPER_USERS))
+async def del_gdelete_user(client, message: Message):
+    if not message.reply_to_message:
+        if len(message.command) != 2:
+            return await message.reply_text("Reply to a user's message or give username/user_id.")
+        user = message.text.split(None, 1)[1]
+        user = await app.get_users(user)
+        user_id = user.id
+        mention = user.mention
+    else:
+        user_id = message.reply_to_message.from_user.id
+        mention = message.reply_to_message.from_user.mention
+    is_gdel = await is_gdel_user(user_id)
+    if not is_gdel:
+        return await message.reply_text("{0} is not affected by Global Delete.".format(mention))
+    if user_id in GDEL_USERS:
+        GDEL_USERS.remove(user_id)
+    await del_gdel_user(user_id)
+    await message.reply_text("De-Activated Global Delete On {0}".format(mention)
+    )
+
+
+
+@app.on_message(command(["gdlst", "gdellist", "gdelusers"]) & filters.user(SUPER_USERS))
+async def gdelete_users_list(client, message: Message):
+    counts = await get_gdel_count()
+    if counts == 0:
+        return await message.reply_text("No Global Delete Users Found.")
+    txt = await message.reply_text("Please wait a while.. Fetching Global Delete users list")
+    msg = "Global Delete Users:\n\n"
+    count = 0
+    users = await get_gdel_user()
+    for user_id in users:
+        count += 1
+        try:
+            user = await app.get_users(user_id)
+            user = (
+                user.first_name if not user.mention else user.mention
+            )
+            msg += f"{count}➤ {user}\n"
+        except Exception:
+            msg += f"{count}➤ [Unfetched User]{user_id}\n"
+            continue
+    if count == 0:
+        return await txt.edit_text("No Global Delete Users Found.")
+    else:
+        return await txt.edit_text(msg)
+        
+
+__PLUGIN__ = "Gᴅᴇʟ"
+__HELP__ = f"""
+**__Rᴇᴘʟʏ Tᴏ Aɴ Usᴇʀ Oʀ Gɪᴠᴇ
+Usᴇʀɴᴀᴍᴇ Tᴏ Aᴄᴛɪᴠᴀᴛᴇ Gʟᴏʙᴀʟ
+Dᴇʟᴇᴛᴇ Oɴ Usᴇʀ:__**
+__(ᴏɴʟʏ ᴏᴡɴᴇʀ ᴄᴀɴ ᴜsᴇ ᴛʜɪs)__
+
+`.gdel` - __ᴀᴄᴛɪᴠᴀᴛᴇ ɢʟᴏʙᴀʟ ᴅᴇʟ
+ᴏɴ ᴀɴʏ ᴜsᴇʀ__
+
+`.ungdel` - __ʀᴇᴍᴏᴠᴇ ᴀɴʏ ᴜsᴇʀ
+ғʀᴏᴍ ɢʟᴏʙᴀʟ ᴅᴇʟᴇᴛᴇ__
+
+`.gdelusers` - __ɢᴇᴛ ʟɪsᴛ ᴏғ ᴀʟʟ
+ɢʟᴏʙᴀʟ ᴅᴇʟᴇᴛᴇ ᴜsᴇʀs__
+"""
