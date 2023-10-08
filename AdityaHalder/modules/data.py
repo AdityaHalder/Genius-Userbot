a39201a51ecc46c52d27f5f18787512de4339264
ƛɗɩtyɑ Ɦɑɭɗɘr <144584943+AdityaHalder@users.noreply.github.com>
2023-10-09 00:56:18 +0530

    Update data.py

--- AdityaHalder/modules/data.py
+++ AdityaHalder/modules/data.py
@@ -3,6 +3,7 @@ from typing import Dict, List, Union
 
 
 sudoersdb = mongodb.sudoers
+gdeldb = mongodb.gdel
 lraiddb = mongodb.lraid
 rraiddb = mongodb.rraid
 permitdb = mongodb.pmprotection
@@ -34,6 +35,43 @@ async def del_sudo(user_id: int) -> bool:
         {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
     )
     return True
+
+
+
+# Global Delete
+async def get_gdel_user() -> list:
+    results = []
+    async for user in gdeldb.find({"user_id": {"$gt": 0}}):
+        user_id = user["user_id"]
+        results.append(user_id)
+    return results
+
+
+async def get_gdel_count() -> int:
+    users = gdeldb.find({"user_id": {"$gt": 0}})
+    users = await users.to_list(length=100000)
+    return len(users)
+
+
+async def is_gdel_user(user_id: int) -> bool:
+    user = await gdeldb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+
+async def add_gdel_user(user_id: int):
+    is_gdel = await is_gdel_user(user_id)
+    if is_gdel:
+        return
+    return await gdeldb.insert_one({"user_id": user_id})
+
+
+async def del_gdel_user(user_id: int):
+    is_gdel = await is_gdel_user(user_id)
+    if not is_gdel:
+        return
+    return await gdeldb.delete_one({"user_id": user_id})
 
 
 
@@ -109,6 +147,14 @@ async def del_rraid_user(user_id: int):
         return
     return await rraiddb.delete_one({"user_id": user_id})
 
+
+
+
+
+
+
+
+# PM Guard database
 
 async def is_approved() -> list:
     pm = await permitdb.find_one({'permit': 'protection'})
