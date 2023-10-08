a9561a6e17348c649e12bc18da0da6f6fd8edbce
ƛɗɩtyɑ Ɦɑɭɗɘr <144584943+AdityaHalder@users.noreply.github.com>
2023-10-09 01:24:52 +0530

    Update check.py

--- AdityaHalder/plugins/check.py
+++ AdityaHalder/plugins/check.py
@@ -9,7 +9,7 @@ from ..modules.data import is_gdel_user
 @app.on_message(filters.incoming, group=2)
 async def actions_checker(client, message):
     user_id = message.from_user.id
-    if await is_gdel_user(user):
+    if await is_gdel_user(user_id):
         try:
             await message.delete()
         except FloodWait as e:
