53f2949b772163a75d49091a80bb881d56a1e6aa
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2024-05-03 13:33:31 +0530

    Update joinvc.py

--- AdityaHalder/plugins/vcbot/joinvc.py
+++ AdityaHalder/plugins/vcbot/joinvc.py
@@ -9,13 +9,13 @@ from pytgcalls.exceptions import GroupCallNotFound
 async def join_vc(client, message):
     chat_id = message.chat.id
     try:
-        a = await call.get_call(chat_id)
-        if (a.status == "not_playing"
-            or a.status == "playing"
-            or a.status == "paused"
-        ):
-            await eor(message, "**Already Joined!**")
-    except GroupCallNotFound:
+        #a = await call.get_call(chat_id)
+        #if (a.status == "not_playing"
+         #   or a.status == "playing"
+           # or a.status == "paused"
+       # ):
+            #await eor(message, "**Already Joined!**")
+    #except GroupCallNotFound:
         await call.join_group_call(chat_id)
         await eor(message, "**Joined VC!**")
     except Exception as e:
