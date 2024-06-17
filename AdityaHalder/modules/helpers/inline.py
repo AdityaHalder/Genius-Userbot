b5d2b79a1243076de4f4edcc233d903c34479acd
Ainulk6 <160497639+Ainulk6@users.noreply.github.com>
2024-06-17 22:11:54 +0530

    Update inline.py

--- AdityaHalder/modules/helpers/inline.py
+++ AdityaHalder/modules/helpers/inline.py
@@ -26,7 +26,7 @@ Genius Userbot » {__version__} ✨...
 Click On Below 🌺 Buttons To
 Get Userbot Commands.
 
-🌷Powered By : [Aditya Server](https://t.me/AdityaServer).**
+🌷Powered By : [ᴀʀᴍᴀɴ ᴋʜᴀɴ](https://t.me/ak_arman_7).**
             """,
             reply_markup=InlineKeyboardMarkup(button),
         )
@@ -47,7 +47,7 @@ Genius Userbot » {__version__} ✨...
 Click On Below 🌺 Buttons To
 Get Userbot Commands.
 
-🌷Powered By : [Aditya Server](https://t.me/AdityaServer).**""",
+🌷Powered By : [ᴀʀᴍᴀɴ ᴋʜᴀɴ](https://t.me/ak_arman_7).**""",
             disable_web_page_preview=True
             ),
             reply_markup=InlineKeyboardMarkup(button),
